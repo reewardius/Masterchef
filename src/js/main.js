e9bd33a28ae441e1287caca8b5df27e080f59eb6
Pirate Puma <kikefontanlorenzo@gmail.com>
2020-09-14 16:38:28 +0200

    :mag: Search modules!

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,22 @@
+// Search Ingredients
+
+function search() {
+    const filter = $('#search').val().toUpperCase();
+    $('.categories>li').each(function() {
+        let show = false;
+        $(this).children('ul').first().children('li').each(function() {
+            const module = $(this).children('.name').first().text().toUpperCase();
+            if(module.indexOf(filter) > -1) {
+                show = true;
+                $(this).css('display', 'block');
+            } else {
+                $(this).css('display', 'none');
+            }
+        });
+        $(this).css('display', show ? 'block' : 'none');
+    });
+}
+
 // Drag, Drop & Sortable Ingredients
 
 $(".categories>li>ul").each(function() {
