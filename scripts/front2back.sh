b1eb1f1eaf6a82d66269a9a38d3f8b7629be16ea
Pirate Puma <kikefontanlorenzo@gmail.com>
2020-10-13 12:47:01 +0200

    :truck: Index 2 Public

--- scripts/front2back.sh
+++ scripts/front2back.sh
@@ -5,14 +5,17 @@ root=$(dirname "${0}")/..
 # Compile frontend
 npm run --prefix "${root}/frontend" compile 1>/dev/null || exit 1
 # Copy to backend
-cat > "${root}/pkg/index.go" << EOF 
-package pkg
+cat > "${root}/pkg/public/index.go" << EOF 
+package public
 
 // ====================
 //  GLOBALS
 // ====================
 
-const source = \`$(cat "${root}/frontend/dist/index.html")\`
+// Source code (Frontend: HTML + CSS + JS) in Go Template format to allow the use of variables
+// Current variables:
+// - .Addr :: Web socket server address
+const Source = \`$(cat "${root}/frontend/dist/index.html")\`
 EOF
 # Output
 echo "[+] Frontend generated ($(du -h "${root}/frontend/dist/index.html" | awk '{ print $1 }'))"
